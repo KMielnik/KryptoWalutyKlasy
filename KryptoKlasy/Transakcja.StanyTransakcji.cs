5f7c41962a778f74927a1555f0a84db3259b99c2
kamilspiewak <kamilspiewak97@gmail.com>
2019-05-16 14:12:33 +0200

    Dodano stany do transakcji

---
+++ KryptoKlasy/Transakcja.StanyTransakcji.cs
@@ -0,0 +1,14 @@
+﻿namespace KryptoKlasy
+{
+    public partial class Transakcja
+    {
+        public enum StanyTransakcji
+        {
+            Nowa,
+            Uzupelniona,
+            Potwierdzona,
+            Odrzucona,
+            Zakończona
+        }
+    }
+}
--- KryptoKlasy/Transakcja.cs
+++ KryptoKlasy/Transakcja.cs
@@ -3,12 +3,13 @@ using System.Text;
 
 namespace KryptoKlasy
 {
-    public class Transakcja
+    public partial class Transakcja
     {
         public DateTime TimeStamp { get; private set; }
         public Uzytkownik Nadawca { get; private set; }
         public Uzytkownik Odbiorca { get; private set; }
         public Portfel.Waluta Waluta { get; private set; }
         public float Kwota { get; private set; }
+        public StanyTransakcji Stan { get; private set; }
     }
 }
